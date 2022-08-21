22079f756e67e3d1bf2b1a047979aa771262950e
amodeose <amodeosa@gmail.com>
2022-08-20 21:58:33 -0400

    show basic weather data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Card from './UI/Card';
 import Button from './UI/Button';
 import key from './key';
+import { useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState("");
+  const [dataExists, setDataExists] = useState(false);
 
   const lat = 43.707310971342956;
   const lon = -79.51579538343675;
@@ -13,7 +17,9 @@ function App() {
     fetch(URL).then(response => {
       return response.json();
     }).then(data => {
-      console.log(data)
+      console.log(data.main);
+      setData(data.main);
+      setDataExists(true);
     })
   }
 
@@ -21,7 +27,14 @@ function App() {
     <div>
       <Card>
         <Button onClick={fetchWeatherHandler}>Get Weather</Button>
-        <h1>Hello</h1>
+        {dataExists && 
+        <div>
+          <p><strong>Current Temperature: </strong>{data.temp}</p>
+          <p><strong>Maximum Temperature: </strong>{data.temp_max}</p>
+          <p><strong>Minimum Temperature: </strong>{data.temp_min}</p>
+          <p><strong>Current Humidity: </strong>{data.humidity}%</p>
+        </div>
+        }
       </Card>
     </div>
   );
