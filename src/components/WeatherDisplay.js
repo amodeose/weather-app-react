c0b8c50cbbd8f0a39543bdb7e7d8ab48da0181dd
amodeose <amodeosa@gmail.com>
2022-09-04 19:37:34 -0400

    add keys to map and better loading logic

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,30 +1,33 @@
 import classes from "./WeatherDisplay.module.css";
 import Card from "../UI/Card";
 import WeatherCard from "./WeatherCard";
-import { useEffect } from "react";
 
 const WeatherDisplay = (props) => {
-  let forecastCards = "";
+  
+  if (props.isLoading) {
+    return (
+      <Card className={classes.loading}>
+        <h3>Loading...</h3>
+      </Card>
+    )
+  };
+
+  let forecastCards;
 
   if (props.forecast != null) {
     let hours = 0;
-    forecastCards = props.forecast.list.map((item) => {
+    forecastCards = props.forecast.list.map((item, index) => {
       hours = hours + 3;
-      return <WeatherCard weatherInfo={item} hours={hours} />;
+      return <WeatherCard key={index} weatherInfo={item} hours={hours} />;
     });
   }
 
   return (
     <main className={classes.main}>
-      {props.isLoading && (
-        <Card className={classes.loading}>
-          <h3>Loading...</h3>
-        </Card>
-      )}
-      {!props.isLoading && props.currentWeather != null && (
+      {props.currentWeather != null && (
         <WeatherCard weatherInfo={props.currentWeather} hours={null}/>
       )}
-      {!props.isLoading && props.forecast != null && forecastCards}
+      {props.forecast != null && forecastCards}
     </main>
   );
 };
